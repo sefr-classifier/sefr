fa096dd62b5dedeca6ad42fbcda6226ae1d8157b
ErfanThinker <erfan.molaei@gmail.com>
2020-06-11 00:45:56 +0430

    Update SEFR.py
    Changed it to a class. The prediction will be much faster now.

--- SEFR.py
+++ SEFR.py
@@ -2,66 +2,68 @@ import numpy as np
 import pandas as pd
 import statistics
 
-weights = []
-bias = 0
 
-# This is used for training the classifier on data. train_predictors are the main data, and train_target are the labels. 
-# The labels should be 0 or 1.
-def fit(train_predictors, train_target):
+class SEFR:
 
-    # poslabels are those records where the label is positive
-    # neglabels are those records where the label is negative
+    def __init__(self):
+        self.weights = []
+        self.bias = 0
 
-    poslabels = train_target == 1
-    neglabels = train_target == 0
 
-    # dfpos are the data where the labels are positive
-    # dfneg are the data where the labels are negative
-    dfpos = train_predictors.loc[poslabels] 
-    dfneg = train_predictors.loc[neglabels] 
 
-    # avgpos is the average value of each feature where the label is positive
-    # avgneg is the average value of each feature where the label is negative
-    avgpos = dfpos.mean(skipna=True) # Eq. 3
-    avgneg = dfneg.mean(skipna=True) # Eq. 4
+    def fit(self, train_predictors, train_target):
+        '''
+        This is used for training the classifier on data.
+        :param train_predictors are the main data in DataFrame
+        :param train_target(labels) should be 0 or 1.
+        '''
+        # poslabels are those records where the label is positive
+        # neglabels are those records where the label is negative
+        poslabels = train_target == 1
+        neglabels = train_target == 0
 
-    # weights are calculated based on Eq. 3 and Eq. 4
-    global weights
-    weights = (avgpos - avgneg) / (avgpos + avgneg) #Eq. 5
-    weights.fillna(0, inplace=True)
+        # dfpos are the data where the labels are positive
+        # dfneg are the data where the labels are negative
+        dfpos = train_predictors.loc[poslabels]
+        dfneg = train_predictors.loc[neglabels]
 
-    posscore = []
-    negscore = []
+        # avgpos is the average value of each feature where the label is positive
+        # avgneg is the average value of each feature where the label is negative
+        avgpos = dfpos.mean(skipna=True)  # Eq. 3
+        avgneg = dfneg.mean(skipna=True)  # Eq. 4
 
-        
-    # For each record, a score is calculated. If the record is positive/negative, the score will be added to posscore/negscore
-    for i in range(len(train_predictors.index)):
-        temp = np.dot(weights, train_predictors.iloc[i,:]) #Eq. 6
-        if train_target.iloc[i] == 0: 
-            negscore.append(temp)
-        if train_target.iloc[i] == 1: 
-            posscore.append(temp)
+        # weights are calculated based on Eq. 3 and Eq. 4
 
-    # posscoreavg and negscoreavg are average values of records scores for positive and negative classes
-    posscoreavg = statistics.mean(posscore) # Eq. 7
-    negscoreavg = statistics.mean(negscore) # Eq. 8
+        self.weights = (avgpos - avgneg) / (avgpos + avgneg)  # Eq. 5
+        self.weights.fillna(0, inplace=True)
 
-    # bias is calculated using a weighted average
-    global bias
+        posscore = []
+        negscore = []
 
-    bias = (len(negscore) * posscoreavg + len(posscore) * negscoreavg) / (len(negscore) + len(posscore)) # Eq. 9
+        # For each record, a score is calculated. If the record is positive/negative, the score will be added to posscore/negscore
+        for i in range(len(train_predictors.index)):
+            temp = np.dot(self.weights, train_predictors.iloc[i, :])  # Eq. 6
+            if train_target.iloc[i] == 0:
+                negscore.append(temp)
+            if train_target.iloc[i] == 1:
+                posscore.append(temp)
 
-    
-# This is for prediction. When the model is learned, it cal be applied on the test data. test_predictors are the data without labels.
-def predict(test_predictors):
+        # posscoreavg and negscoreavg are average values of records scores for positive and negative classes
+        posscoreavg = statistics.mean(posscore)  # Eq. 7
+        negscoreavg = statistics.mean(negscore)  # Eq. 8
 
-    pred = []
-    temp = 0
+        # bias is calculated using a weighted average
 
-    for i in range(len(test_predictors)):
-        temp = np.dot(weights, (test_predictors.iloc[i]))
-        if temp <= bias: pred.append(0)
-        if temp > bias: pred.append(1)
+        self.bias = (len(negscore) * posscoreavg + len(posscore) * negscoreavg) / (len(negscore) + len(posscore))  # Eq. 9
 
-        # The list of predicted labels are returned
-    return pred
+
+
+    def predict(self, test_predictors):
+        '''
+        This is for prediction. When the model is trained, it can be applied on the test data.
+        :param test_predictors: the data without labels in DataFrame
+        :return: List of predictions
+        '''
+        temp = np.dot(self.weights, test_predictors.to_numpy())
+        preds = np.where(temp <= self.bias, 0 , 1)
+        return preds
